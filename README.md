9df681fb6e8e379119e6137d72f32e2401f42ba2
Paul Walsh <twentytwentyk@live.com>
2017-02-16 17:47:49 -0500

    done

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Your First (Several) Asynchronous Requests
 
 [See assignment in Alexa.](https://alexa.bitmaker.co/cohorts/67/assignments/2055/latest)
+
+Step 1
+1. /pets
+2. Post
+3. 
--- ajax.js
+++ ajax.js
@@ -1,5 +1,58 @@
 $(document).ready(function () {
+var url = 'http://first-ajax-api.herokuapp.com/'
+  $("#step12").click(function(){
+    $.ajax({
+      url ,
+      method: 'GET',
+      data: "",
+      dataType: 'text'
+    })
+  });
+  $("#step3456").click(function() {
+    $.ajax({
+      url: `${url}ping`,
+      method: 'GET',
+      dataType: 'text'
+    }).done(
+      function () {
+        $("#step3456").append("Wow what a ride! I came all the way from the server!")
+      }
+    ).fail(function(){
+      $("#step3456").append("OOPS! 404 try harder!")
+    }).always(function(){
+      console.log("Sup Bro? Your request is finished!");
+    })
+  });
 
-  /* Your code goes here */
+  $("#step7").click(function() {
+    $.ajax({
+      url: `${url}count`,
+      method: 'GET',
+      dataType: 'text'
+    }).done(function(responseData) {
+      console.log(responseData);
+    })
+  });
 
+  $("#step8").click(function(){
+    $.ajax({
+      url: `${url}time`,
+      method: 'GET',
+      data: {timezone: 'America/Mexico_City'},
+      dataType: 'text'
+    }).done(function(responseData){
+      console.log(responseData);
+    })
+  });
+
+  $("#step9").click(function(){
+    $.ajax({
+      url: `${url}a_car`,
+      method: 'GET',
+      dataType: 'html'
+    }).done(function(responseData){
+      console.log(responseData);
+      $("#list").append(responseData)
+    })
+  });
 });
